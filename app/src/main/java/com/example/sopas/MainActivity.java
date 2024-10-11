1d5714b0915734b4a90df356ce83ab0428b916d0
Diego Aragón <diegofelipearagongarcia@gmail.com>
2024-10-10 19:23:37 -0600

    Animacion +  menú

--- app/src/main/java/com/example/sopas/MainActivity.java
+++ app/src/main/java/com/example/sopas/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.sopas;
 
+
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,22 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        // Configurar los insets para manejar los márgenes del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Programar el inicio de la actividad Home después de 5 segundos
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, homeActivity.class);
-                startActivity(intent);
-                finish();  // Cierra la MainActivity para que no se pueda volver al pulsar el botón "atrás"
+                startActivity(new Intent(MainActivity.this,homeActivity.class));
+                finish();
             }
-        }, 5000);  // 5000 milisegundos = 5 segundos
+        }, 1500);
     }
 }
